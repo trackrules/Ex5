14c0591c3bbd3df865bd05ebc1c5963889432241
trackrules <trackrules.ksl@gmail.com>
2023-06-02 15:03:46 +1200

    q1 fucked with

--- ex5-kernel/parallel_task.c
+++ ex5-kernel/parallel_task.c
@@ -1,11 +1,11 @@
 #include "wramp.h"
 
-void base10(int value)
+void base10(int value, int base)
 {   
-    WrampParallel-> UpperLeftSSD    = (value/1000)%10;
-	WrampParallel-> UpperRightSSD   = (value/100)%10;
-	WrampParallel-> LowerLeftSSD    = (value/10)%10;
-	WrampParallel-> LowerRightSSD   = value%10;
+    WrampParallel-> UpperLeftSSD    = (value/base^3)%base;
+	WrampParallel-> UpperRightSSD   = (value/base^2)%base;
+	WrampParallel-> LowerLeftSSD    = (value/base^1)%base;
+	WrampParallel-> LowerRightSSD   = (value/base^0)%base;
 }
 
 void base16(int value)
@@ -20,6 +20,8 @@ void main()
 {
     int switches = 0;
     int buttons = 0;
+    int base = 0;
+
     while (1)
     {
         switches = WrampParallel->Switches;
@@ -29,11 +31,13 @@ void main()
         // WrampParallel->LowerRightSSD = buttons;
         if (buttons == 1)
         {
-            base10(switches);
+            base = 10;
+            base10(switches,base);
         }
         if (buttons == 2)
         {
-            base16(switches);
+            base = 0x10;
+            base10(switches,base);
         }
         if (buttons == 4)
         {
