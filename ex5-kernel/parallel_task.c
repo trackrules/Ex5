6660734793d3cf93a9cfcdf328a660744757dbc6
Kaio <trackrules.ksl@gmail.com>
2023-06-02 14:16:46 +1200

    q1 base 10 done

--- ex5-kernel/parallel_task.c
+++ ex5-kernel/parallel_task.c
@@ -1,13 +1,29 @@
 #include "wramp.h"
 
+void base10(int value)
+{
+    WrampParallel-> UpperLeftSSD    = (value/1000)%10;
+	WrampParallel-> UpperRightSSD   = (value/100)%10;
+	WrampParallel-> LowerLeftSSD    = (value/10)%10;
+	WrampParallel-> LowerRightSSD   = value%10;
+}
+
 void main()  
 {
     int switches = 0;
-
+    int buttons = 0;
     while (1)
     {
         switches = WrampParallel->Switches;
-        WrampParallel->LowerRightSSD = switches;
+        // WrampParallel->LowerRightSSD = switches;
+
+        buttons = WrampParallel->Buttons;
+        // WrampParallel->LowerRightSSD = buttons;
+        if (buttons == 1)
+        {
+            base10(switches);
+        }
+        
     }
     
 }
--- ex5-kernel/parallel_task.s
+++ ex5-kernel/parallel_task.s
@@ -1,22 +1,68 @@
-.global	main
+.global	base10
 .text
-main:
-	subui	$sp, $sp, 2
-	sw	$7, 0($sp)
-	sw	$13, 1($sp)
-	addu	$7, $0, $0
-	j	L.7
-L.6:
+base10:
+	subui	$sp, $sp, 3
+	sw	$11, 0($sp)
+	sw	$12, 1($sp)
+	sw	$13, 2($sp)
 	lhi	$13, 0x7
-	ori	$13, $13, 0x3000
-	lw	$7, 0($13)
+	ori	$13, $13, 0x3006
+	lw	$12, 3($sp)
+	divi	$12, $12, 1000
+	remi	$12, $12, 10
+	sw	$12, 0($13)
+	lhi	$13, 0x7
+	ori	$13, $13, 0x3007
+	lw	$12, 3($sp)
+	divi	$12, $12, 100
+	remi	$12, $12, 10
+	sw	$12, 0($13)
+	addui	$13, $0, 10
+	lhi	$12, 0x7
+	ori	$12, $12, 0x3008
+	lw	$11, 3($sp)
+	div	$11, $11, $13
+	rem	$13, $11, $13
+	sw	$13, 0($12)
 	lhi	$13, 0x7
 	ori	$13, $13, 0x3009
-	sw	$7, 0($13)
-L.7:
-	j	L.6
+	lw	$12, 3($sp)
+	remi	$12, $12, 10
+	sw	$12, 0($13)
 L.5:
-	lw	$7, 0($sp)
-	lw	$13, 1($sp)
-	addui	$sp, $sp, 2
+	lw	$11, 0($sp)
+	lw	$12, 1($sp)
+	lw	$13, 2($sp)
+	addui	$sp, $sp, 3
+	jr	$ra
+.global	main
+main:
+	subui	$sp, $sp, 5
+	sw	$6, 1($sp)
+	sw	$7, 2($sp)
+	sw	$13, 3($sp)
+	sw	$ra, 4($sp)
+	addu	$6, $0, $0
+	addu	$7, $0, $0
+	j	L.8
+L.7:
+	lhi	$13, 0x7
+	ori	$13, $13, 0x3000
+	lw	$6, 0($13)
+	lhi	$13, 0x7
+	ori	$13, $13, 0x3001
+	lw	$7, 0($13)
+	snei	$13, $7, 1
+	bnez	$13, L.10
+	sw	$6, 0($sp)
+	jal	base10
+L.10:
+L.8:
+	j	L.7
+L.6:
+	lw	$6, 1($sp)
+	lw	$7, 2($sp)
+	lw	$13, 3($sp)
+	lw	$ra, 4($sp)
+	addui	$sp, $sp, 5
 	jr	$ra
