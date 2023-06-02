9fcdb6eb589251c6d99b183eeede150ac046491b
Kaio <trackrules.ksl@gmail.com>
2023-06-02 14:38:01 +1200

    q1 done

--- ex5-kernel/parallel_task.c
+++ ex5-kernel/parallel_task.c
@@ -1,11 +1,19 @@
 #include "wramp.h"
 
 void base10(int value)
-{
+{   
     WrampParallel-> UpperLeftSSD    = (value/1000)%10;
 	WrampParallel-> UpperRightSSD   = (value/100)%10;
 	WrampParallel-> LowerLeftSSD    = (value/10)%10;
 	WrampParallel-> LowerRightSSD   = value%10;
+}
+
+void base16(int value)
+{
+     WrampParallel-> UpperLeftSSD    = (value/0x1000)%0x10;
+	WrampParallel-> UpperRightSSD   = (value/0x100)%0x10;
+	WrampParallel-> LowerLeftSSD    = (value/0x10)%0x10;
+	WrampParallel-> LowerRightSSD   = value%0x10;
 }
 
 void main()  
@@ -22,6 +30,14 @@ void main()
         if (buttons == 1)
         {
             base10(switches);
+        }
+        if (buttons == 2)
+        {
+            base16(switches);
+        }
+        if (buttons == 4)
+        {
+            return;
         }
         
     }
--- ex5-kernel/parallel_task.s
+++ ex5-kernel/parallel_task.s
@@ -35,6 +35,42 @@ L.5:
 	lw	$13, 2($sp)
 	addui	$sp, $sp, 3
 	jr	$ra
+.global	base16
+base16:
+	subui	$sp, $sp, 3
+	sw	$11, 0($sp)
+	sw	$12, 1($sp)
+	sw	$13, 2($sp)
+	lhi	$13, 0x7
+	ori	$13, $13, 0x3006
+	lw	$12, 3($sp)
+	divi	$12, $12, 4096
+	remi	$12, $12, 16
+	sw	$12, 0($13)
+	lhi	$13, 0x7
+	ori	$13, $13, 0x3007
+	lw	$12, 3($sp)
+	divi	$12, $12, 256
+	remi	$12, $12, 16
+	sw	$12, 0($13)
+	addui	$13, $0, 16
+	lhi	$12, 0x7
+	ori	$12, $12, 0x3008
+	lw	$11, 3($sp)
+	div	$11, $11, $13
+	rem	$13, $11, $13
+	sw	$13, 0($12)
+	lhi	$13, 0x7
+	ori	$13, $13, 0x3009
+	lw	$12, 3($sp)
+	remi	$12, $12, 16
+	sw	$12, 0($13)
+L.6:
+	lw	$11, 0($sp)
+	lw	$12, 1($sp)
+	lw	$13, 2($sp)
+	addui	$sp, $sp, 3
+	jr	$ra
 .global	main
 main:
 	subui	$sp, $sp, 5
@@ -44,8 +80,8 @@ main:
 	sw	$ra, 4($sp)
 	addu	$6, $0, $0
 	addu	$7, $0, $0
-	j	L.8
-L.7:
+	j	L.9
+L.8:
 	lhi	$13, 0x7
 	ori	$13, $13, 0x3000
 	lw	$6, 0($13)
@@ -53,13 +89,22 @@ L.7:
 	ori	$13, $13, 0x3001
 	lw	$7, 0($13)
 	snei	$13, $7, 1
-	bnez	$13, L.10
+	bnez	$13, L.11
 	sw	$6, 0($sp)
 	jal	base10
-L.10:
-L.8:
+L.11:
+	snei	$13, $7, 2
+	bnez	$13, L.13
+	sw	$6, 0($sp)
+	jal	base16
+L.13:
+	snei	$13, $7, 4
+	bnez	$13, L.15
 	j	L.7
-L.6:
+L.15:
+L.9:
+	j	L.8
+L.7:
 	lw	$6, 1($sp)
 	lw	$7, 2($sp)
 	lw	$13, 3($sp)
