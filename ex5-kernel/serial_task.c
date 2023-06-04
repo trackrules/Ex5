6d85755ec5cca676038d06baa332683378251f96
trackrules <trackrules.ksl@gmail.com>
2023-06-04 21:27:30 +1200

    q2 working

--- ex5-kernel/serial_task.c
+++ ex5-kernel/serial_task.c
@@ -1,18 +1,61 @@
 #include "wramp.h"
 
-int counter = 26;
-char r = '\r';
+int counter;
+int pressed = '1';
 
-void poll(){
-    while (WrampSp2->Stat!=2){}
+void putc(int c)
+{
+	while(!(WrampSp2->Stat & 2));
+	WrampSp2->Tx = c;
 }
-void main(){
 
+void main(){
     while (1)
     {
-        poll();
-        WrampSp2 -> Tx = counter;
-
-        counter = counter +1;
+        while (!(WrampSp2->Stat & 1))
+        {
+            if (pressed == '1')
+            {
+                putc('\r');
+                putc(((counter/1000000)%10)+0x30);
+                putc(((counter/100000)%10)+0x30);
+                putc(((counter/10000)%10)+0x30);
+                putc(((counter/1000)%10)+0x30);
+                putc('.');
+                putc(((counter/100)%10)+0x30);
+                putc(((counter/10)%10)+0x30);
+            }
+            else if (pressed == '2')
+            {
+                int minutes = counter/100/60;
+                int seconds = counter/100;
+                putc('\r');
+                putc(((minutes/10)%10)+0x30);
+                putc(((minutes)%10)+0x30);
+                putc(':');
+                putc(((seconds/10)%6)+0x30);
+                putc((seconds%10)+0x30);
+                putc(0x20);
+                putc(0x20);
+            }
+            else if (pressed == '3')
+            {
+                putc('\r');
+                putc(((counter/100000)%10)+0x30);
+                putc(((counter/10000)%10)+0x30);
+                putc(((counter/1000)%10)+0x30);
+                putc(((counter/100)%10)+0x30);
+                putc(((counter/10)%10)+0x30);
+                putc(((counter/1)%10)+0x30);
+                
+            }
+            else if (pressed == 'q')
+            {
+                return;
+            }
+            
+            
+        }
+        pressed = WrampSp2->Rx;
     }
 }
--- ex5-kernel/serial_task.s
+++ ex5-kernel/serial_task.s
@@ -1,46 +1,207 @@
 .global	counter
 counter:
-	.word	0x1a
-.global	r
-r:
-	.word	0xd
-.global	poll
+	.word	0x2f4b7
+.global	pressed
+pressed:
+	.word	0x31
+.global	putc
 .text
-poll:
-	subui	$sp, $sp, 1
-	sw	$13, 0($sp)
+putc:
+	subui	$sp, $sp, 2
+	sw	$12, 0($sp)
+	sw	$13, 1($sp)
 L.6:
 L.7:
 	lhi	$13, 0x7
 	ori	$13, $13, 0x1003
 	lw	$13, 0($13)
-	snei	$13, $13, 2
+	andi	$13, $13, 2
+	seq	$13, $13, $0
 	bnez	$13, L.6
+	lhi	$13, 0x7
+	ori	$13, $13, 0x1000
+	lw	$12, 2($sp)
+	sw	$12, 0($13)
 L.5:
-	lw	$13, 0($sp)
-	addui	$sp, $sp, 1
+	lw	$12, 0($sp)
+	lw	$13, 1($sp)
+	addui	$sp, $sp, 2
 	jr	$ra
 .global	main
 main:
-	subui	$sp, $sp, 3
-	sw	$12, 0($sp)
-	sw	$13, 1($sp)
-	sw	$ra, 2($sp)
+	subui	$sp, $sp, 6
+	sw	$6, 1($sp)
+	sw	$7, 2($sp)
+	sw	$12, 3($sp)
+	sw	$13, 4($sp)
+	sw	$ra, 5($sp)
 	j	L.11
-L.10:
-	jal	poll
-	lhi	$13, 0x7
-	ori	$13, $13, 0x1000
-	lw	$12, counter($0)
-	sw	$12, 0($13)
+L.13:
+	lw	$13, pressed($0)
+	snei	$13, $13, 49
+	bnez	$13, L.16
+	addui	$13, $0, 13
+	sw	$13, 0($sp)
+	jal	putc
 	lw	$13, counter($0)
-	addi	$13, $13, 1
-	sw	$13, counter($0)
+	lhi	$12, 0xf
+	ori	$12, $12, 0x4240
+	div	$13, $13, $12
+	remi	$13, $13, 10
+	addi	$13, $13, 48
+	sw	$13, 0($sp)
+	jal	putc
+	lw	$13, counter($0)
+	lhi	$12, 0x1
+	ori	$12, $12, 0x86a0
+	div	$13, $13, $12
+	remi	$13, $13, 10
+	addi	$13, $13, 48
+	sw	$13, 0($sp)
+	jal	putc
+	lw	$13, counter($0)
+	divi	$13, $13, 10000
+	remi	$13, $13, 10
+	addi	$13, $13, 48
+	sw	$13, 0($sp)
+	jal	putc
+	lw	$13, counter($0)
+	divi	$13, $13, 1000
+	remi	$13, $13, 10
+	addi	$13, $13, 48
+	sw	$13, 0($sp)
+	jal	putc
+	addui	$13, $0, 46
+	sw	$13, 0($sp)
+	jal	putc
+	lw	$13, counter($0)
+	divi	$13, $13, 100
+	remi	$13, $13, 10
+	addi	$13, $13, 48
+	sw	$13, 0($sp)
+	jal	putc
+	addui	$13, $0, 10
+	lw	$12, counter($0)
+	div	$12, $12, $13
+	rem	$13, $12, $13
+	addi	$13, $13, 48
+	sw	$13, 0($sp)
+	jal	putc
+	j	L.17
+L.16:
+	lw	$13, pressed($0)
+	snei	$13, $13, 50
+	bnez	$13, L.18
+	lw	$13, counter($0)
+	divi	$13, $13, 100
+	divi	$7, $13, 60
+	lw	$13, counter($0)
+	divi	$6, $13, 100
+	addui	$13, $0, 13
+	sw	$13, 0($sp)
+	jal	putc
+	addui	$13, $0, 10
+	div	$12, $7, $13
+	rem	$13, $12, $13
+	addi	$13, $13, 48
+	sw	$13, 0($sp)
+	jal	putc
+	remi	$13, $7, 10
+	addi	$13, $13, 48
+	sw	$13, 0($sp)
+	jal	putc
+	addui	$13, $0, 58
+	sw	$13, 0($sp)
+	jal	putc
+	divi	$13, $6, 10
+	remi	$13, $13, 6
+	addi	$13, $13, 48
+	sw	$13, 0($sp)
+	jal	putc
+	remi	$13, $6, 10
+	addi	$13, $13, 48
+	sw	$13, 0($sp)
+	jal	putc
+	addui	$13, $0, 32
+	sw	$13, 0($sp)
+	jal	putc
+	addui	$13, $0, 32
+	sw	$13, 0($sp)
+	jal	putc
+	j	L.19
+L.18:
+	lw	$13, pressed($0)
+	snei	$13, $13, 51
+	bnez	$13, L.20
+	addui	$13, $0, 13
+	sw	$13, 0($sp)
+	jal	putc
+	lw	$13, counter($0)
+	lhi	$12, 0x1
+	ori	$12, $12, 0x86a0
+	div	$13, $13, $12
+	remi	$13, $13, 10
+	addi	$13, $13, 48
+	sw	$13, 0($sp)
+	jal	putc
+	lw	$13, counter($0)
+	divi	$13, $13, 10000
+	remi	$13, $13, 10
+	addi	$13, $13, 48
+	sw	$13, 0($sp)
+	jal	putc
+	lw	$13, counter($0)
+	divi	$13, $13, 1000
+	remi	$13, $13, 10
+	addi	$13, $13, 48
+	sw	$13, 0($sp)
+	jal	putc
+	lw	$13, counter($0)
+	divi	$13, $13, 100
+	remi	$13, $13, 10
+	addi	$13, $13, 48
+	sw	$13, 0($sp)
+	jal	putc
+	addui	$13, $0, 10
+	lw	$12, counter($0)
+	div	$12, $12, $13
+	rem	$13, $12, $13
+	addi	$13, $13, 48
+	sw	$13, 0($sp)
+	jal	putc
+	lw	$13, counter($0)
+	remi	$13, $13, 10
+	addi	$13, $13, 48
+	sw	$13, 0($sp)
+	jal	putc
+	j	L.21
+L.20:
+	lw	$13, pressed($0)
+	snei	$13, $13, 113
+	bnez	$13, L.22
+	j	L.9
+L.22:
+L.21:
+L.19:
+L.17:
+L.14:
+	lhi	$13, 0x7
+	ori	$13, $13, 0x1003
+	lw	$13, 0($13)
+	andi	$13, $13, 1
+	seq	$13, $13, $0
+	bnez	$13, L.13
+	lhi	$13, 0x7
+	ori	$13, $13, 0x1001
+	lw	$13, 0($13)
+	sw	$13, pressed($0)
 L.11:
-	j	L.10
+	j	L.14
 L.9:
-	lw	$12, 0($sp)
-	lw	$13, 1($sp)
-	lw	$ra, 2($sp)
-	addui	$sp, $sp, 3
+	lw	$6, 1($sp)
+	lw	$7, 2($sp)
+	lw	$12, 3($sp)
+	lw	$13, 4($sp)
+	lw	$ra, 5($sp)
+	addui	$sp, $sp, 6
 	jr	$ra
